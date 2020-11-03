c0dec73e806db4b70974b6075d0fc5c6bf464472
ZelDaCarDeveloperPage <ivan.pyzhyznov.90@mail.ru>
2020-11-03 23:09:01 +0300

    Pupa

---
+++ lab1/TPOlab0/Source.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#define SIZE 20
+
+using namespace std;
+
+int cmp(int t1, int t2) {
+	return t1 - t2;
+}
+void printArray(int* arr, int size) {
+	if (arr == NULL) return;
+	for (int i = 0; i < size; i++) cout << arr[i] << " ";
+	cout << endl;
+	return;
+}
+
+template<typename T> T* BubSort(T* input, int size, int((cmp)(T t1, T t2)));
+template<typename T> T* InsertSort(T* input, int size, int((cmp)(T t1, T t2)));
+template<typename T> T* PyrSort(T* input, int size, int((cmp)(T t1, T t2)));
+template<typename T> void SiftDown(T* input, int size, int root, int bottom, int((cmp)(T t1, T t2)));
+template <typename T> T* SelecSort(T* input, int size, int((cmp)(T t1, T t2)));
+
+
+int main(void)
+{
+	setlocale(LC_ALL, "Russian");
+
+	int inputArray[SIZE] = { 1,4,1,65,12,32,12,32,
+							4,-4,21,-5,7,7,44,12,
+							-24, 42, 11,4 };
+	//string inputArray[SIZE] = { "AA", "BTE", "WEBV", "EBR", "ERBE" ,"ERBE", "ERB", "AAB", "FAS", "ZBZ", "REE"};
+	int choice;
+	cout << "Input > ";
+	printArray(inputArray, SIZE);
+	cout << "0. Пузырьковая\n1. Выборкой\n2. Вставками\n3. Пирамида" << endl;
+	cout << "Введите номер сортировки (0..3) > ";
+
+	
+	cin >> choice;
+	int* res = NULL;
+	switch (choice) {
+	case 0:
+		res = BubSort(inputArray, SIZE, cmp);
+		break;
+	case 1:
+		res = SelecSort(inputArray, SIZE, cmp);
+		break;
+	case 2:
+		res = InsertSort(inputArray, SIZE, cmp);
+		break;
+	case 3:
+		res = PyrSort(inputArray, SIZE, cmp);
+		break;
+	default:
+		break;
+	}
+	cout << "Output < ";
+	printArray(res, SIZE);
+	system("pause");
+	return 0;
+}
+
+template<typename T>
+T* BubSort(T* input, int size, int((cmp)(T t1, T t2))) {
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - i - 1; j++) {
+			if (cmp(input[j], input[j + 1]) > 0) {
+				T b = input[j];
+				input[j] = input[j + 1];
+				input[j + 1] = b;
+			}
+		}
+	}
+	return input;
+}
+template<typename T>
+T* InsertSort(T* input, int size, int((cmp)(T t1, T t2))) {
+	T temp;
+	int tempIndx;
+	for (int i = 1; i < size; i++) {
+		temp = input[i];
+		tempIndx = i - 1;
+		while (tempIndx >= 0 && cmp(input[tempIndx], temp) < 0) {
+			input[tempIndx + 1] = input[tempIndx];
+			input[tempIndx] = temp;
+			tempIndx--;
+		}
+	}
+	return input;
+}
+template<typename T>
+T* PyrSort(T* input, int size, int((cmp)(T t1, T t2))) {
+	for (int i = (size / 2) - 1; i >= 0; i--) {
+		SiftDown(input, size, i, size - 1, cmp);
+	}
+	for (int i = size - 1; i >= 1; i--) {
+		T temp = input[0];
+		input[0] = input[i];
+		input[i] = temp;
+		SiftDown(input, size, 0, i - 1, cmp);
+	}
+	return input;
+}
+template<typename T>
+void SiftDown(T* input, int size, int root, int bottom, int((cmp)(T t1, T t2))) {
+	int maxElem;
+	bool done = false;
+	while ((root * 2 <= bottom) && (!done)) {
+		if (root * 2 == bottom) {
+			maxElem = root * 2;
+		}
+		else if (cmp(input[root * 2], input[root * 2 + 1]) > 0) {
+			maxElem = root * 2;
+		}
+		else {
+			maxElem = root * 2 + 1;
+		}
+		if (cmp(input[root], input[maxElem]) < 0) {
+			T temp = input[root];
+			input[root] = input[maxElem];
+			input[maxElem] = temp;
+			root = maxElem;
+		}
+		else {
+			done = true;
+		}
+	}
+}
+template <typename T>
+T* SelecSort(T* input, int size, int((cmp)(T t1, T t2))) {
+	for (int i = 0; i < size - 1; i++) {
+		int indx1 = i;
+		for (int j = i + 1; j < size; j++) {
+			if (cmp(input[j], input[indx1]) > 0) {
+				indx1 = j;
+			}
+		}
+		T temp = input[i];
+		input[i] = input[indx1];
+		input[indx1] = temp;
+	}
+	return input;
+}
--- lab1/TPOlab1/Source.cpp
+++ lab1/TPOlab1/Source.cpp
@@ -3,60 +3,60 @@
 #include "SelectionSorter.h"
 #include "InsertionSorter.h"
 #include "PyramidSorter.h"
-#define SIZE 11
+#define SIZE 20
 
 using namespace std;
 
-int cmp(string t1, string t2) {
-	int size = t1.size() <= t2.size() ? t1.size() : t2.size();
+int cmp(int t1, int t2) {
+	/*int size = t1.size() <= t2.size() ? t1.size() : t2.size();
 	for (int i = 0; i < size; i++) {
 		if (t1[i] < t2[i]) return 1;
 		else if (t1[i] > t2[i]) return -1;
 	}
 	if (t1.size() < size) return 1;
-	else return -1;
-	//return t1 - t2;
+	else return -1;*/
+	return t1 - t2;
 }
 
 int main(void) 
 {
 	setlocale(LC_ALL, "Russian");
 	
-	//int inputArray[SIZE] = {1,4,1,65,12,32,12,32,
-	//						4,-4,21,-5,7,7,44,12,
-	//						-24, 42, 11,4};
-	string inputArray[SIZE] = { "AA", "BTE", "WEBV", "EBR", "ERBE" ,"ERBE", "ERB", "AAB", "FAS", "ZBZ", "REE"};
+	int inputArray[SIZE] = {1,4,1,65,12,32,12,32,
+							4,-4,21,-5,7,7,44,12,
+							-24, 42, 11,4};
+	//string inputArray[SIZE] = { "AA", "BTE", "WEBV", "EBR", "ERBE" ,"ERBE", "ERB", "AAB", "FAS", "ZBZ", "REE"};
 	int choice;
 	cout << "0. Пузырьковая\n1. Выборкой\n2. Вставками\n3.Пирамида" << endl;
 	cout << "Введите номер сортировки (0..2) > ";
 	cin >> choice;
-	Sorter<string>* _sorter;
+	Sorter<int>* _sorter;
 
 	switch (choice) {
 	case 0:
 		cout << "BUBBLE SORT" << endl;
-		_sorter = new BubbleSorter<string>(SIZE);
+		_sorter = new BubbleSorter<int>(SIZE);
 		_sorter->Print(inputArray);
 		_sorter->Sort(inputArray, cmp);
 		_sorter->Print(inputArray);
 		break;
 	case 1:
 		cout << "SELECTION SORT" << endl;
-		_sorter = new SelectionSorter<string>(SIZE);
+		_sorter = new SelectionSorter<int>(SIZE);
 		_sorter->Print(inputArray);
 		_sorter->Sort(inputArray, cmp);
 		_sorter->Print(inputArray);
 		break;
 	case 2:
 		cout << "INSERTION SORT" << endl;
-		_sorter = new InsertionSorter<string>(SIZE);
+		_sorter = new InsertionSorter<int>(SIZE);
 		_sorter->Print(inputArray);
 		_sorter->Sort(inputArray, cmp);
 		_sorter->Print(inputArray);
 		break;
 	case 3:
 		cout << "PYRAMID SORT" << endl;
-		_sorter = new PyramidSorter<string>(SIZE);
+		_sorter = new PyramidSorter<int>(SIZE);
 		_sorter->Print(inputArray);
 		_sorter->Sort(inputArray, cmp);
 		_sorter->Print(inputArray);
